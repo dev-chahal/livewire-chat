6faa7cef145c2e11a75512c9825ce83463f32d2a
dev-chahal <dev-chahal@users.noreply.github.com>
2022-08-10 06:30:21 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace DevChahal\LivewireChat\Tests;
 
+use DevChahal\LivewireChat\LivewireChatServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use DevChahal\LivewireChat\LivewireChatServiceProvider;
 
 class TestCase extends Orchestra
 {
